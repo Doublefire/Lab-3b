9eb4db8c92ad6f1ec119a02ffcbf857181d7e5a8
Doublefire <70965630+Doublefire@users.noreply.github.com>
2020-10-13 00:02:19 +0200

    13/10-2020

---
+++ Q11/cmake-build-debug/CMakeFiles/Q11.dir/DependInfo.cmake
@@ -0,0 +1,21 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "C"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_C
+  "C:/Users/Adam/Documents/GitHub/Lab-3b/Q11/gradient.c" "C:/Users/Adam/Documents/GitHub/Lab-3b/Q11/cmake-build-debug/CMakeFiles/Q11.dir/gradient.c.obj"
+  "C:/Users/Adam/Documents/GitHub/Lab-3b/Q11/tga.c" "C:/Users/Adam/Documents/GitHub/Lab-3b/Q11/cmake-build-debug/CMakeFiles/Q11.dir/tga.c.obj"
+  )
+set(CMAKE_C_COMPILER_ID "GNU")
+
+# The include file search paths:
+set(CMAKE_C_TARGET_INCLUDE_PATH
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ Q11/cmake-build-debug/CMakeFiles/Q11.dir/cmake_clean.cmake
@@ -0,0 +1,13 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Q11.dir/gradient.c.obj"
+  "CMakeFiles/Q11.dir/tga.c.obj"
+  "Q11.exe"
+  "Q11.exe.manifest"
+  "Q11.pdb"
+  "libQ11.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/Q11.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
